2aa2317f22ea605dade11a96836204895b4ff5a9
Mithil Hassan <mithilhassan007@gmail.com>
2023-03-25 09:08:16 +0600

    modified

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ gsap
   .fromTo(".banner_background", { opacity: 0 }, { opacity: 1 });
 
 ScrollTrigger.matchMedia({
-  "(min-width: 992px)": function () {
+  "(min-width: 1440px)": function () {
     gsap
       .timeline({
         scrollTrigger: {
@@ -81,6 +81,7 @@ ScrollTrigger.matchMedia({
           scrub: 2,
           start: "top 90%",
           end: "2500px",
+
           toggleActions: "restart none none none",
         },
       })
@@ -96,7 +97,7 @@ ScrollTrigger.matchMedia({
       .to(".letter1, .letter2, .banner_title", { display: "none", duration: 0 })
       .to(".banner_content .justify_between", { margin: 0, duration: 0 }, "<")
       .to(".line_wrapper", { width: "900px", duration: 1 })
-      .to(".animated_line1", { height: 300, border: 20, duration: 2 }, "<")
+      .to(".animated_line1", { height: 300, border: 18, duration: 1 }, "<")
       .to(".line_wrapper", {
         width: 360,
         backgroundImage:
@@ -110,14 +111,64 @@ ScrollTrigger.matchMedia({
           "linear-gradient(45deg, rgba(235,13,186,1) 0%, rgba(235,13,186,1) 100%)",
       })
       .to(".line_wrapper", {
-        width: 20,
+        width: 18,
         backgroundImage:
           "linear-gradient(45deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 100%)",
-        duration: 2,
+        duration: 1,
+        delay: 1,
       })
-      .to(".animated_line2", { opacity: 1, delay: 0.5 })
-      .to(".animated_line2", { width: 400 })
-      .to(".animated_line1", { height: 0, delay: 0.5 })
+      .to(".animated_line2", { opacity: 1, width: 400 })
+      .to(".animated_line1", { height: 0, delay: 1 })
+      .to(".animated_line1", { opacity: 0 }, ">")
+      .to(".animated_line2", { width: 0, delay: 0.5 })
+      .to(".animated_line2", { opacity: 0 }, ">");
+  },
+  "(min-width: 992px) and (max-width: 1439px)": function () {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#navbar",
+          scrub: 2,
+          start: "top 90%",
+          end: "2200px",
+
+          toggleActions: "restart none none none",
+        },
+      })
+      .to(".letter1, .letter2, .banner_title", { opacity: 0 })
+      .to(".line_wrapper", { width: "25%" })
+      .to(".animated_line1", { backgroundColor: "#8343D8", duration: 1 })
+      .to(".animated_line1", {
+        backgroundColor: "transparent",
+        height: 0,
+        border: "10px solid #8343D8",
+        duration: 0,
+      })
+      .to(".letter1, .letter2, .banner_title", { display: "none", duration: 0 })
+      .to(".banner_content .justify_between", { margin: 0, duration: 0 }, "<")
+      .to(".line_wrapper", { width: "900px", duration: 1 })
+      .to(".animated_line1", { height: 300, border: 18, duration: 1 }, "<")
+      .to(".line_wrapper", {
+        width: 360,
+        backgroundImage:
+          "linear-gradient(45deg, rgba(149,69,251,1) 0%, rgba(235,13,186,1) 100%)",
+        duration: 2,
+        delay: 1,
+      })
+      .to(".animated_line1", { height: 360, borderWidth: 0, duration: 2 }, "<")
+      .to(".line_wrapper", {
+        backgroundImage:
+          "linear-gradient(45deg, rgba(235,13,186,1) 0%, rgba(235,13,186,1) 100%)",
+      })
+      .to(".line_wrapper", {
+        width: 18,
+        backgroundImage:
+          "linear-gradient(45deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 100%)",
+        duration: 1,
+        delay: 1,
+      })
+      .to(".animated_line2", { opacity: 1, width: 400 })
+      .to(".animated_line1", { height: 0, delay: 1 })
       .to(".animated_line1", { opacity: 0 }, ">")
       .to(".animated_line2", { width: 0, delay: 0.5 })
       .to(".animated_line2", { opacity: 0 }, ">");
@@ -129,8 +180,7 @@ ScrollTrigger.matchMedia({
           trigger: "#navbar",
           scrub: 2,
           start: "top 90%",
-          end: "3200px",
-          // markers: true,
+          end: "3250px",
           toggleActions: "restart none none none",
         },
       })
